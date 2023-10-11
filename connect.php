3617eefa5e08a1d1f46111b102b0596e5347c7da
PapaCologne <jphigomera0619@gmail.com>
2023-10-11 17:21:34 +0800

    OCTOBER 11, 2023

--- connect.php
+++ connect.php
@@ -4,8 +4,4 @@
 	$db_password= "";
 	$db_host= "localhost";
 	$db_database= "pcnhrs";
-	$link=mysql_connect($db_host, $db_user, $db_password) or die ("Connection Failure");
-	
-	mysql_select_db($db_database, $link);
-	
-	?>
+	$link  = mysqli_connect($db_host, $db_user, $db_password, $db_database) or die ("Connection Failure");
--- printapp.php
+++ printapp.php
@@ -1,24 +1,21 @@
-
-<?php  
+<?php
 //export.php  
 
 include("connect.php");
-    session_start();
+session_start();
 
 date_default_timezone_set('Asia/Hong_Kong');
-$date = date('D : F d, Y'); 
+$date = date('D : F d, Y');
 
- $dtnow=date("m/d/Y");
-
-
+$dtnow = date("m/d/Y");
 
 
 
-if(isset($_POST['Back']))
-   {
-      
-       header("location:recruitment.php");
 
+
+if (isset($_POST['Back'])) {
+
+  header("location:recruitment.php");
 }
 
 
@@ -27,231 +24,255 @@ if(isset($_POST['Back']))
 ?>
 
 <html>
+
 <head>
+  <meta http-equiv="X-UA-Compatible" content="IE=7">
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <!-- Bootstrap 5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-<style type="text/css">
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter&family=Julius+Sans+One&family=Poppins&family=Quicksand:wght@400;500;800&family=Roboto&family=Thasadith&display=swap" rel="stylesheet">
 
-p {
-  text-align: right;
-  padding: 0px 15% 0px 0px ;
-}
+  <style type="text/css">
+    * {
+      font-family: 'Inter', sans-serif;
+      font-weight: 800;
+    }
 
-.body5025p
-{
-position:absolute;
-top: 1px;
-left: 20%;
-border: 0px solid green;
-height: 90%;
-width: 60%;
-}
+    .form-label {
+      text-align: left !important;
+      /* padding: 0px 15% 0px 0px; */
+    }
 
-.body50
-{
-position:absolute;
-top: inherit;
-left: 0%;
-border: 0px solid black;
-height: 100%;
-width: 30%;
-}
+    .body5025p {
+      position: absolute;
+      top: 1px;
+      left: 20%;
+      border: 0px solid green;
+      height: 90%;
+      width: 60%;
+    }
 
-.body60
-{
-position:absolute;
-top: inherit;
-left: 25%;
-border: 0px solid black;
-height: 100%;
-width: 50%;
-}
-.many1 
-{
-  position:absolute;
-  left: 0%;
-  top: 0px;
-  color:black;
-  width: 100%;
-    z-index: 100;
-  border: 0px inset Blue;
-  opacity: .8;
+    .body50 {
+      position: absolute;
+      top: inherit;
+      left: 0%;
+      border: 0px solid black;
+      height: 100%;
+      width: 30%;
+    }
 
-  padding: 0% 7% 0% 3%;
-  background-size: cover;
-  border-radius: 0px;
-  box-shadow: 0px 0px 0px 0px #000000;
-  text-align: right;
+    .body60 {
+      position: absolute;
+      top: inherit;
+      left: 25%;
+      border: 0px solid black;
+      height: 100%;
+      width: 50%;
+    }
 
-  font-family: AR berkley medium; 
-  font-size: 15;
-   
-}
-</style>
-	<title></title>
+    .buttons,
+    .name,
+    .footer {
+      text-align: right;
+    }
+
+    .form-control{
+      border-top: none;
+      border-left: none;
+      border-right: none;
+      border-bottom: 1px solid black;
+      border-radius: 0;
+      margin-top: 1rem;
+    }
+
+    .form-group{
+      margin-top: 1rem !important;
+    }
+    .many1 {
+      position: absolute;
+      left: 0%;
+      top: 0px;
+      color: black;
+      width: 100%;
+      z-index: 100;
+      border: 0px inset Blue;
+      opacity: .8;
+      padding: 0% 7% 0% 3%;
+      background-size: cover;
+      border-radius: 0px;
+      box-shadow: 0px 0px 0px 0px #000000;
+      /* text-align: right; */
+      font-size: 12px;
+    }
+
+    @media screen and (max-width: 840px){
+      .name .name{
+        font-size: 20px;
+      }
+      .name .address, .name .number{
+        font-size: 12px;
+      }
+    }
+  </style>
+  <title></title>
 </head>
 
-<body >   
+<body>
 
-<div class="body5025p">
+  <div class="body5025p">
 
-<?php
+    <?php
 
-//echo $_SESSION["appnoto"];
-$appnoto= $_SESSION["appnoto"];
+    //echo $_SESSION["appnoto"];
+    $appnoto = $_SESSION["appnoto"];
 
-
-   $resultap =mysql_query("SELECT * FROM employees where appno ='$appnoto'");
-      while($rowap=mysql_fetch_array($resultap))
-       {
-      
-echo '
+    $querytap = "SELECT * FROM employees where appno ='$appnoto'";
+    $resultap = mysqli_query($link, $querytap);
+    while ($rowap = mysqli_fetch_array($resultap)) { ?>
 
 
-<div class="many1"><br>
-<form action = "" method = "POST">
-  <button class="button" name="Back" style="float:right;font-size:15;width:150px;height:30px" id="myDIV1" >Back</button>
+      <div class="many1"><br>
+        <form action="" method="POST">
+          <div class="mb-1 buttons">
+            <button class="btn btn-dark" id="myDIV" onclick="myFunction()">Print this page</button>
+            <button class="btn btn-secondary" name="Back" id="myDIV1">Back</button>
+          </div>
+        </form>
 
-  <button class="button" style="float:right;font-size:15;width:150px;height:30px" id="myDIV" onclick="myFunction()">Print this page</button>
-</form>
+        <div class="form-group name">
 
-<div class="form-group" >
+          <center>
+            <img src="<?php echo $rowap[2] ?>" alt="" style="float:left; width:150px;height:150px;">
+          </center>
+          <br><br><br>
 
+          <label class="form-label">
+            <font class="name" color="Black" size="6"><?php echo $rowap[6] .  ", " . $rowap[7] . " " . $rowap[8] ?></font>
+          </label>
+          <br>
+          <label class="form-label address"><?php echo $rowap[10] ?></label>
+          <br>
+          <label class="form-label number"><?php echo $rowap[18] ?></label>
+
+        </div>
+
+        <hr>
         <center>
-                          <img src="'.$rowap[2].'" alt="" style="float:left; width:150px;height:150px;">
-                          </center>
-                          <br><br><br>
+          <label class="form-label">
+            <font color="Black" size="4">Applicant Information</font>
+          </label>
+        </center>
+        <hr>
 
-        <label><font color="Black" size="6">'.$rowap[6].",".$rowap[7]." ".$rowap[8].'</font></label>
+        <form action="" class="row">
+          <div class="col-md-12 mt-3">
+            <label class="form-labe">Applicant Number:</label>
+            <input type="text" name="newshortlist" id="number" value="<?php echo $rowap[4] ?>" class="form-controls" placeholder="" readonly>
+          </div>
+          <div class="form-group col-md-12">
+            <label class="form-label">Region:</label>
+            <input type="text" name="newshortlist" value="<?php echo $rowap[12] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+          </div>
+          <div class="form-group col-md-12">
+            <label class="form-label">Gender:</label>
+            <input type="text" name="newshortlist" value="<?php echo $rowap[16] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+          </div>
+          <div class="form-group col-md-12">
+            <label class="form-label">Birthdate:</label>
+            <input type="text" name="newshortlist" value="<?php echo $rowap[14] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+          </div>
+          <div class="form-group col-md-12">
+            <label class="form-label">Civil Status:</label>
+            <input type="text" name="newshortlist" value="<?php echo $rowap[17] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+          </div>
+          <div class="form-group col-md-12">
+            <label class="form-label">SSS Number:</label>
+            <input type="text" name="newshortlist" value="<?php echo $rowap[24] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+          </div>
+          <div class="form-group col-md-12">
+            <label class="form-label">Philhealth Number:</label>
+            <input type="text" name="newshortlist" value="<?php echo $rowap[26] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+          </div>
+          <div class="form-group col-md-12">
+            <label class="form-label">Pag-ibig Number:</label>
+            <input type="text" name="newshortlist" value="<?php echo $rowap[25] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+          </div>
+          <div class="form-group col-md-12">
+            <label class="form-label">TIN Number:</label>
+            <input type="text" name="newshortlist" value="<?php echo $rowap[27] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+
+            <div class="form-group col-md-12">
+              <label class="form-label">Desired Position:</label>
+              <input type="text" name="newshortlist" value="<?php echo $rowap[21] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+
+            </div>
+            <div class="form-group col-md-12">
+              <label class="form-label">Email Address:</label>
+              <input type="text" name="newshortlist" value="<?php echo $rowap[20] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+
+            </div>
+            <div class="form-group col-md-12">
+              <label class="form-label">Joined Date:</label>
+              <input type="text" name="newshortlist" value="<?php echo $rowap[3] ?>" class="form-controls" placeholder="" style="height:35px;width:60%;" readonly>
+
+            </div>
+        </form>
         <br>
-        <label>'.$rowap[10].'</label>
-           <br>
-        <label>'.$rowap[18].'</label>
-  
-       </div>
+        <hr>
 
 
-       <hr>
-       <center>
-          <label><font color="Black" size="4">Applicant Information</font></label>
-       </center>
-         <hr>
+        <label style="float:left; width:400px;height:10px;">I hereby certify that the above information are true and correct.</label>
+
+        <br><br>
+
+        <div class="footer">
+          <label class="form-label">___________________________</label><br>
+          <label class="form-label">Signature over Printed Name</label>
+          <br><br>
+          <label class="form-label">________________</label><br>
+          <label class="form-label">Date</label>
+        </div>
 
 
 
-
-
-	<div class="form-group">
-	 <p> <label >Applicant Number:</label>
-  		<input type="text" name = "newshortlist" value= "'.$rowap[4].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly><p>
-    </div>
-	<div class="form-group" >
-       <p> <label>Region:</label>
-  		<input type="text" name = "newshortlist" value= "'.$rowap[12].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-    	<div class="form-group" >
-       <p> <label>Gender:</label>
-  	  		<input type="text" name = "newshortlist" value= "'.$rowap[16].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-    	<div class="form-group" >
-      <p>  <label>Birthdate:</label>
-  	  		<input type="text" name = "newshortlist" value= "'.$rowap[14].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-	<div class="form-group" >
-       <p> <label>Civil Status:</label>
-  		<input type="text" name = "newshortlist" value= "'.$rowap[17].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-    	<div class="form-group" >
-       <p> <label>SSS Number:</label>
-  		<input type="text" name = "newshortlist" value= "'.$rowap[24].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-   	<div class="form-group" >
-       <p> <label>Philhealth Number:</label>
-  		<input type="text" name = "newshortlist" value= "'.$rowap[26].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-    	<div class="form-group" >
-       <p> <label>Pag-ibig Number:</label>
-  		<input type="text" name = "newshortlist" value= "'.$rowap[25].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-    	<div class="form-group" >
-       <p> <label>TIN Number:</label>
-  		<input type="text" name = "newshortlist" value= "'.$rowap[27].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-   
-    	<div class="form-group" >
-       <p> <label>Desired Position:</label>
-    		<input type="text" name = "newshortlist" value= "'.$rowap[21].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-	<div class="form-group" >
-       <p> <label>Email Address:</label>
-  		<input type="text" name = "newshortlist" value= "'.$rowap[20].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-    	<div class="form-group" >
-       <p> <label>Joined Date:</label>
-  		<input type="text" name = "newshortlist" value= "'.$rowap[3].'" class="form-group"  placeholder="" style= "height:35px;width:60%;" readonly></p>
-    </div>
-
-<hr>
-
-
-      <label style="float:left; width:400px;height:10px;">I hereby certify that the above information are true and correct.</label>
-      
-<br><br>
-
-<label>___________________________</label><br>
- <label>Signature over Printed Name</label>
-<br><br>
-<label>________________</label><br>
- <label>Date</label>
+      <?php
+      $rowap[10];
+    }
+      ?>
 
 
 
 
-';      
-       
-  $rowap[10];
-
-
-
-
-
-
-
-
-      }
-?>
-
-
-
-
-</div>
+      </div>
 
 </body>
+
 </html>
 
 <script>
+  function myFunction() {
+    var x = document.getElementById("myDIV");
+    var y = document.getElementById("myDIV1");
 
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "block";
 
+    } else {
 
+      x.style.display = "none";
+      y.style.display = "none";
 
-
-
-
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  var y = document.getElementById("myDIV1");
-  
-  if (x.style.display === "none") {
-    x.style.display = "block";
-   y.style.display = "block";
-   
-  } else {
-
-    x.style.display = "none";
-    y.style.display = "none";
-    
-     window.print();
-         x.style.display = "block";
-   y.style.display = "block";
+      window.print();
+      x.style.display = "block";
+      y.style.display = "block";
+    }
   }
-}
 </script>
